884f9646faca2a923e350c1eaf6389818f88f33f
harrison <harrisonwellsl@outlook.com>
2021-01-23 13:38:37 +0800

    编写部分TCPserver代码

--- net_assistant/tcpserver.cpp
+++ net_assistant/tcpserver.cpp
@@ -1,12 +1,44 @@
 #include "tcpserver.h"
 
+/**
+ * @brief tcpServer::tcpServer
+ * @param ip
+ * @param port
+ * 构造函数
+ */
 tcpServer::tcpServer(const QString &ip, unsigned short port)
 {
-    this->ip = ip;
+    this->ip = new QHostAddress(ip);
     this->port = port;
+    QObject::connect(this->pTcpServer, SIGNAL(newConnection(), )
 }
 
+/**
+ * @brief tcpServer::tcpServerOpen
+ * @return
+ * 打开TCP服务器
+ */
 int tcpServer::tcpServerOpen()
 {
-    QHostAddress pHostAddress = QHostAddress(this->port);
+    this->pTcpServer = new QTcpServer(nullptr);
+    pTcpServer->setMaxPendingConnections(TCP_SERVER_MAX_CONNECTS);
+    return 0;
+}
+
+/**
+ * @brief tcpServer::acceptConnection
+ * 当链接到来的时候补充pTcpSocketConnection参数
+ */
+void tcpServer::acceptConnection()
+{
+    this->pTcpSocketConnection = this->pTcpServer->nextPendingConnection();
+}
+
+tcpServer::~tcpServer()
+{
+    if (this->pTcpServer->isListening())
+        this->pTcpServer->close();
+
+    delete this->pTcpServer;
+    delete this->ip;
 }
--- net_assistant/tcpserver.h
+++ net_assistant/tcpserver.h
@@ -1,18 +1,26 @@
 #ifndef TCPSERVER_H
 #define TCPSERVER_H
 
+#include <QTcpSocket>
 #include <QTcpServer>
 #include <QString>
+
+#define TCP_SERVER_MAX_CONNECTS 20
 
 class tcpServer {
 public:
     tcpServer(const QString& ip, unsigned short port);
-    ~tcpServer() = default;
+    ~tcpServer();
     int tcpServerOpen();
 
 private:
-    QString ip;
+    QHostAddress *ip;
+    QTcpServer *pTcpServer;
+    QTcpSocket *pTcpSocketConnection;
     unsigned port;
+
+private slots:
+    void acceptConnection();
 };
 
 #endif // TCPSERVER_H
